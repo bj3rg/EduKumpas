a5c3d9993e1a6b8f7a87cf3b9e915c4cd0b27be2
Angelo Burog <angelokburog30@gmail.com>
2024-05-01 15:28:13 +0800

    fix: remove post method for user and add login view

--- EduKumpas/my_edukumpas/api/views.py
+++ EduKumpas/my_edukumpas/api/views.py
@@ -3,17 +3,41 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password, check_password
 from django.http import JsonResponse, HttpResponse
 from .serializers import *
+from django.db.models import Q  
 from .models import Schools,ProgramsOffered, Admission, Facilities, Activities, Clubs, News, FeaturesHighlights
 # Create your views here.
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Schools
 from .models import *
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 CustomUser = get_user_model()
 print(make_password('12345'))
 print(check_password('1234','pbkdf2_sha256$720000$K9qlERHbhsZvGPXH8EFdJC$Hwo4oaNiBtC3UKtYmRlUzipdgtYIrzaEjph7J2bhQBo=' ))
+
+
+class LoginView(APIView):
+    def post(self, request):
+        identifier = request.data.get('identifier')  # This can be either username or email
+        password = request.data.get('password')
+
+        if not identifier or not password:
+            return Response('Please provide both identifier and password.', status=status.HTTP_400_BAD_REQUEST)
+
+        user = get_object_or_404(User, Q(username=identifier) | Q(email=identifier))
+
+        if not user.check_password(password):
+            return Response('Incorrect identifier or password', status=status.HTTP_401_UNAUTHORIZED)
+
+        token, _ = Token.objects.get_or_create(user=user)
+        serializer = UserSerializer(user)
+        return Response({"token": token.key, "user": serializer.data['username']}, status=status.HTTP_200_OK)
+
 class RepresentativeList(APIView):
     def post(self,request):
         body = request.data
@@ -23,7 +47,14 @@ class RepresentativeList(APIView):
             ).first()
             
             if existing_school:
-                return Response({'error': 'School already registered'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response('School already registered', status=status.HTTP_400_BAD_REQUEST)
+            
+            existing_email = User.objects.filter(
+                email = body['email_address']
+            ).exists()
+            
+            if existing_email:
+                return Response("Email already registered", status=status.HTTP_400_BAD_REQUEST)
             
             new_user = User.objects.create_user(
                 username=body['username'],
@@ -59,6 +90,8 @@ class RepresentativeList(APIView):
         
 
 class SchoolListView(APIView):
+    # authentication_classes = [SessionAuthentication, TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
     def get(self, request):
         school_type = request.query_params.get('school_type')
         if school_type:
@@ -67,38 +100,40 @@ class SchoolListView(APIView):
             schools = Schools.objects.all()
         data = list(schools.values('id','school_name', 'school_type', 'school_website', 'public_private', 'school_representative', 'school_rep_phone_num', 'school_rep_email', 'school_logo', 'school_image', 'school_location'))
         return Response(data)
-    def post(self, request):
-        body = request.data
-        try:
-            school_name = body['school_name']
-            school_type = body.get('school_type', '')
-            school_website = body.get('school_website', '')
-            public_private = body.get('public_private', '')
-            school_representative = body.get('school_representative', '')
-            school_rep_phone_num = body.get('school_rep_phone_num', '')
-            school_rep_email = body.get('school_rep_email', '')
-            school_logo = body.get('school_logo', None)
-            school_image = body.get('school_image', None)
+    # def post(self, request):
+    #     body = request.data
+    #     try:
+    #         school_name = body['school_name']
+    #         school_type = body.get('school_type', '')
+    #         school_website = body.get('school_website', '')
+    #         public_private = body.get('public_private', '')
+    #         school_representative = body.get('school_representative', '')
+    #         school_rep_phone_num = body.get('school_rep_phone_num', '')
+    #         school_rep_email = body.get('school_rep_email', '')
+    #         school_logo = body.get('school_logo', None)
+    #         school_image = body.get('school_image', None)
             
 
-            new_school = Schools(
-                school_name=school_name,
-                school_type=school_type,
-                school_website=school_website,
-                public_private=public_private,
-                school_representative=school_representative,
-                school_rep_phone_num=school_rep_phone_num,
-                school_rep_email=school_rep_email,
-                school_logo=school_logo,
-                school_image=school_image
-            )
-            new_school.save()
+    #         new_school = Schools(
+    #             school_name=school_name,
+    #             school_type=school_type,
+    #             school_website=school_website,
+    #             public_private=public_private,
+    #             school_representative=school_representative,
+    #             school_rep_phone_num=school_rep_phone_num,
+    #             school_rep_email=school_rep_email,
+    #             school_logo=school_logo,
+    #             school_image=school_image
+    #         )
+    #         new_school.save()
 
-            return Response({'message': 'School added successfully'}, status=status.HTTP_201_CREATED)
-        except KeyError:
-            return Response({'error': 'Missing required fields'}, status=status.HTTP_400_BAD_REQUEST)
+    #         return Response({'message': 'School added successfully'}, status=status.HTTP_201_CREATED)
+    #     except KeyError:
+    #         return Response({'error': 'Missing required fields'}, status=status.HTTP_400_BAD_REQUEST)
 
 class SchoolListViewByID(APIView):
+    # authentication_classes = [SessionAuthentication, TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
     def get(self, request,pk):
         school= Schools.objects.filter(id=pk)
         serializer= SchoolsSerializer(school, many=True)
@@ -114,12 +149,12 @@ class OfferedListView(APIView):
             offered = ProgramsOffered.objects.all()
         data = list(offered.values( 'program_name','program_description' , 'duration', 'tuition_fee'))
         return Response(data)
-    def post(self, request):
-        serializer = ProgramsOfferedSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def post(self, request):
+    #     serializer = ProgramsOfferedSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class ActivitiesListView(APIView):
     def get(self, request):
@@ -130,12 +165,12 @@ class ActivitiesListView(APIView):
             activities = Activities.objects.all()
         data = list(activities.values('activity_name', 'activity_description', 'activity_image'))
         return Response(data)
-    def post(self, request):
-        serializer = ActivitiesSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def post(self, request):
+    #     serializer = ActivitiesSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class FacilitiesListView(APIView):
     def get(self, request):
@@ -146,12 +181,12 @@ class FacilitiesListView(APIView):
             facilities = Facilities.objects.all()
         data = list(facilities.values('facility_name', 'facility_description', 'facility_image'))
         return Response(data)
-    def post(self, request):
-        serializer = FacilitiesSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def post(self, request):
+    #     serializer = FacilitiesSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class ClubListView(APIView):
     def get(self, request):
@@ -162,12 +197,12 @@ class ClubListView(APIView):
             clubs = Clubs.objects.all()
         data = list(clubs.values('club_name', 'club_description', 'club_image'))
         return Response(data)
-    def post(self, request):
-        serializer =ClubsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def post(self, request):
+    #     serializer =ClubsSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class FeaturesListView(APIView):
     def get(self, request):
@@ -178,12 +213,12 @@ class FeaturesListView(APIView):
             features = FeaturesHighlights.objects.all()
         data = list(features.values('feature_image'))
         return Response(data)
-    def post(self, request):
-        serializer = FeaturesHighlightsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def post(self, request):
+    #     serializer = FeaturesHighlightsSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class NewsListView(APIView):
     def get(self, request):
@@ -194,10 +229,10 @@ class NewsListView(APIView):
             news = News.objects.all()
         data = list(news.values('news_header', 'news_description', 'news_image'))
         return Response(data)
-    def post(self, request):
-        serializer = NewsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def post(self, request):
+    #     serializer = NewsSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
