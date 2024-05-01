4056696190ec70e2d07ff3ccbeeaf9dacb51ed37
Angelo Burog <angelokburog30@gmail.com>
2024-05-01 15:25:21 +0800

    style: apply responsiveness

--- EduKumpas/react-edukumpas/src/components/mini-components/FieldInput.jsx
+++ EduKumpas/react-edukumpas/src/components/mini-components/FieldInput.jsx
@@ -2,13 +2,11 @@ import React from "react";
 
 function FieldInput({ type, inputDisplay, width, handleChange, value }) {
   return (
-    <div className="text-white text-[14px]">
+    <div className="text-white text-[18px] w-full">
       <h1>{inputDisplay}</h1>
       <input
         type={type}
-        className={
-          "px-[5px] text-black h-[28px] bg-white rounded-[8px] " + width
-        }
+        className={`px-[5px] text-black h-[28px] bg-white rounded-[8px] w-[100%] ${width}`}
         onChange={handleChange}
         value={value}
       />
