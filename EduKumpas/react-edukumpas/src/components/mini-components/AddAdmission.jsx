754c07f9bc25d1db57b66839b1de596242d483e2
Angelo Burog <angelokburog30@gmail.com>
2024-05-02 20:52:50 +0800

    feat: new cards and post methods add

--- EduKumpas/react-edukumpas/src/components/mini-components/AddAdmission.jsx
+++ EduKumpas/react-edukumpas/src/components/mini-components/AddAdmission.jsx
@@ -1,6 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import FieldInput from "../../components/mini-components/FieldInput";
 export const AddAdmission = ({ school_id, school_name }) => {
+  const [invalid, setInvalid] = useState("");
+  const [exist, errorExist] = useState("");
+  const [correct, setCorrect] = useState(false);
+  const [newAdmission, setNewAdmission] = useState({
+    school: school_id || "",
+    name: "",
+    description: "",
+    fee: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("school", newAdmission.school);
+    formData.append("name", newAdmission.name);
+    formData.append("description", newAdmission.description);
+    formData.append("fee", newAdmission.fee);
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/admin/admission/0",
+        formData,
+        {
+          headers: {
+            Authorization: `Token ${sessionStorage.getItem("token")}`,
+          },
+        }
+      );
+      window.location.reload();
+    } catch (error) {
+      if (error.response && error.response.status === 406) {
+        errorExist("Expenses Listed");
+        setInvalid("");
+        setCorrect(true);
+      } else {
+        setInvalid("Complete the fields");
+        errorExist("");
+        setCorrect(true);
+        console.log("Error:", error);
+      }
+    }
+  };
   return (
     <div className="w-full lg:w-[750px] bg-orange-400 mt-500 px-[30px] py-[30px]  rounded-[15px] z-[99]">
       <div className="text-white mb-[10px] flex flex-col gap-[3px]">
@@ -19,12 +62,12 @@ export const AddAdmission = ({ school_id, school_name }) => {
         </div>
         <div className="w-full flex justify-between ">
           <FieldInput
-            inputDisplay={"Requirements"}
+            inputDisplay={"Name"}
             type={"text"}
             handleChange={(e) => {
-              setNewOfficial((prev) => ({
+              setNewAdmission((prev) => ({
                 ...prev,
-                surname: e.target.value,
+                name: e.target.value,
               }));
             }}
           />
@@ -32,12 +75,12 @@ export const AddAdmission = ({ school_id, school_name }) => {
         {/* second row */}
         <div className="w-full flex justify-between ">
           <FieldInput
-            inputDisplay={"Process Guide"}
+            inputDisplay={"Description"}
             type={"text"}
             handleChange={(e) => {
-              setNewOfficial((prev) => ({
+              setNewAdmission((prev) => ({
                 ...prev,
-                address: e.target.value,
+                description: e.target.value,
               }));
             }}
           />
@@ -45,20 +88,30 @@ export const AddAdmission = ({ school_id, school_name }) => {
         {/* third row */}
         <div className="w-full flex justify-between ">
           <FieldInput
-            inputDisplay={"Admission Fee"}
+            inputDisplay={"Fee"}
             type={"number"}
             handleChange={(e) => {
-              setNewOfficial((prev) => ({ ...prev, sex: e.target.value }));
+              setNewAdmission((prev) => ({ ...prev, fee: e.target.value }));
             }}
           />
         </div>
         {/* fourth row */}
-
+        {correct && (
+          <div className="bg-[#F5656561] w-[200px] mt-2 text-sm border-[1px] border-[#FF000061] px-[20px] py-[5px] rounded-md">
+            <h1>
+              {exist}
+              {invalid}
+            </h1>
+          </div>
+        )}
         <div className="w-full flex gap-[20px] mt-5 justify-between">
           <button className="p-[5px] bg-white rounded-[10px] text-black min-w-[70px] cursor-pointer">
             Clear
           </button>
-          <button className="p-[5px] bg-white rounded-[10px] text-black min-w-[70px] cursor-pointer">
+          <button
+            onClick={handleSubmit}
+            className="p-[5px] bg-white rounded-[10px] text-black min-w-[70px] cursor-pointer"
+          >
             Add
           </button>
           {/* <ButtonComp2
--- EduKumpas/react-edukumpas/src/components/pages/ViewAdmin.jsx
+++ EduKumpas/react-edukumpas/src/components/pages/ViewAdmin.jsx
@@ -5,6 +5,7 @@ import editIcon from "../../assets/icons8-edit-50.png";
 import Admin_Navbar from "../mini-components/AdminNavbar";
 export const Admin = () => {
   const [data, setData] = useState([]);
+  const [exp, setExp] = useState([]);
   const [schoolId, setSchoolId] = useState("");
   const [schoolName, setSchoolName] = useState("");
   const navigate = useNavigate();
@@ -30,9 +31,41 @@ export const Admin = () => {
         setSchoolId(school_id);
 
         setData(res.data);
+      });
+    axios
+      .get("http://127.0.0.1:8000/api/admin/admission/0", {
+        headers: {
+          Authorization: `Token ${sessionStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        const expensesData = res.data[0];
+        const school_id = expensesData.id;
+        console.log("Here");
+        console.log(school_id);
+        setExp(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
+
+  const handleDelete = async (admissionId) => {
+    try {
+      await axios.delete(
+        `http://127.0.0.1:8000/api/admin/admission/${admissionId}`,
+        {
+          headers: {
+            Authorization: `Token ${sessionStorage.getItem("token")}`,
+          },
+        }
+      );
+      alert("Admission deleted successfully");
+      window.location.reload();
+    } catch (error) {
+      console.error("Error deleting admission:", error);
+      alert("Error deleting admission");
+    }
+  };
+
   return (
     <div>
       <Admin_Navbar
@@ -42,14 +75,15 @@ export const Admin = () => {
       />
       <div>
         <div className="flex justify-center mt-12">
-          <table className="table-auto">
+          <table className="table-auto border">
             <thead>
               <tr className="header-row text-left">
-                <th className="p-2">name</th>
-                <th className="p-2">website</th>
-                <th className="p-2">public/private</th>
-                <th className="p-2">number</th>
-                <th className="p-2">address</th>
+                <th className="p-2 border border-slate-600">name</th>
+                <th className="p-2 border border-slate-600">website</th>
+                <th className="p-2 border border-slate-600">public/private</th>
+                <th className="p-2 border border-slate-600">number</th>
+                <th className="p-2 border border-slate-600">address</th>
+                <th className="p-2 border border-slate-600"></th>
               </tr>
             </thead>
             <tbody>
@@ -62,6 +96,32 @@ export const Admin = () => {
                   <td className="p-2">{d.school_location}</td>
                   <td className="p-2">
                     <button className="w-8">
+                      <img src={editIcon} alt="" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="flex justify-center mt-12">
+          <table className="table-auto border border-collapse">
+            <thead>
+              <tr className="header-row text-left">
+                <th className="p-2 border border-slate-600">Expenses Name</th>
+                <th className="p-2 border border-slate-600">Description</th>
+                <th className="p-2 border border-slate-600">Cost</th>
+                <th className="border border-slate-600"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {exp.map((d, i) => (
+                <tr key={i}>
+                  <td className="p-2">{d.name}</td>
+                  <td className="p-2">{d.description}</td>
+                  <td className="p-2">{d.fee}</td>
+                  <td className="p-2">
+                    <button onClick={() => handleDelete(d.id)} className="w-8">
                       <img src={editIcon} alt="" />
                     </button>
                   </td>
